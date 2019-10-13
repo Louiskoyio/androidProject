c066aeb364e4a4148580384d0e18e1edfce078fc
Louiskoyio <koyiolouis@gmail.com>
2019-10-13 10:41:49 +0300

    Changed Application Flow

--- app/src/main/java/com/example/mobi/MainActivity.java
+++ app/src/main/java/com/example/mobi/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.mobi.models.Product;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public ArrayList<Product> cart = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
